918526a89463b61704bfe6427e64b491468a5d6f
Suman Shrestha <sumanstha0614@gmail.com>
2025-11-24 16:16:02 +0545

    02 :: Trying google sing in Warning this version is full of errors

--- lib/features/auth/data/firebase_auth_repo.dart
+++ lib/features/auth/data/firebase_auth_repo.dart
@@ -5,6 +5,7 @@
  */
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:makepost/features/auth/domain/models/app_user.dart';
 import 'package:makepost/features/auth/domain/repos/auth_repo.dart';
 
@@ -78,6 +79,37 @@ class FirebaseAuthRepo implements AuthRepo {
       return "Password reset email send !  check you mail";
     } catch (e) {
       return "An error occured while sending email: $e";
+    }
+  }
+
+  @override
+  Future<AppUser?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+      if (gUser == null) {
+        return null;
+      }
+      final GoogleSignInAuthentication gAuth = await gUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: gAuth.accessToken,
+        idToken: gAuth.idToken,
+      );
+
+      UserCredential userCredential = await firebaseAuth.signInWithCredential(
+        credential,
+      );
+      final firebaseUser = userCredential.user;
+      if (firebaseUser == null) {
+        return null;
+      }
+      AppUser user = AppUser(
+        email: firebaseUser.email ?? '',
+        uid: firebaseUser.uid,
+      );
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
     }
   }
 }
